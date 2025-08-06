6109dc7f4836b463dec813f7228bbc2ce0fea580
Anthony Gonzalez <aagonzalez.2@sudamericano.edu.ec>
2025-08-05 19:28:13 -0500

    fix: eliminar prerender para rutas dinámicas, compatible con Vercel

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'support-family/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'medicines/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'profile-patient/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Las demás rutas se prerenderizan
   }
 ];
